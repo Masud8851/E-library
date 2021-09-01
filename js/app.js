c741b3c795c7ab8ef6d62aded22342a86e1dec37
Masud8851 <masud1971.cse@gmail.com>
2021-09-02 01:48:02 +0600

    Assignment

--- js/app.js
+++ js/app.js
@@ -1,55 +1,79 @@
-// Give Error message for empty search
-document.getElementById('error-message').style.display = 'none';
+const details = document.getElementById('details');
+const displayAll = document.getElementById('display');
+
+
+
+// Get Fetch JSON Data
 const searchBook = () => {
-    const searchField = document.getElementById('search-field');
-    const searchText = searchField.value;
-    
-    // clear data
-    searchField.value = '';
+    const input = document.getElementById('input');
+    const searchText = input.value;
+    input.value = '';
+    if (searchText.length == '') {
+        // For Empty Input
+        const countSection = document.getElementById('count');
+        countSection.textContent = '';
+        const h5 = document.createElement('h5');
+        h5.classList.add('text-center');
+        h5.innerText = `Somthing Error Try Again!`;
+        countSection.appendChild(h5);
+        displayAll.textContent = '';
 
-    // Handle empty search request
-    if (searchText == '') {
-        // please write something to display
-
-        displayError();
-    }
-    else{
+    } else {
         fetch(`http://openlibrary.org/search.json?q=${searchText}`)
-        .then(res => res.json())
-        .then(data => displaySearchResult(data.docs));
-    }  
+            .then(res => res.json())
+            .then(data => {
+                // console.log(data)
+                count(data.num_found);
+                displayBooks(data.docs);
+            });
+    }
 }
 
-const displayError = () => {
-    document.getElementById('error-message').style.display = 'block';
-    document.getElementById('book-numbers').textContent = '';
-    document.getElementById('book-details').textContent = '';
-
+// Count Total Result and Error Message
+const count = (count) => {
+    const countSection = document.getElementById('count');
+    countSection.textContent = '';
+    const h5 = document.createElement('h5');
+    h5.classList.add('text-center');
+    if (count == 0) {
+        h5.innerText = `Sorry! We can't found any Book,Please try with Correct name.`;
+    } else {
+        h5.innerText = `Total Book Found : ${count}`;
+    }
+    countSection.appendChild(h5);
 }
 
-const displaySearchResult = books => {
-    console.log(books);
-    const searchResult = document.getElementById('search-result');
+
+
+// display All Results of Book
+const displayBooks = (books) => {
+    displayAll.textContent = '';
     books.forEach(book => {
+        // console.log(book);
         const div = document.createElement('div');
-        div.innerHTML= `
-        <div class="col">
-            <div class="card h-100">
-            <img src="https://covers.openlibrary.org/b/id/${book.cover_i}-L.jpg" class="card-img-top" alt="...">
-            <div class="card-body">
-                <h5 class="">${book.title}</h5>
-                <p class="card-text">${book.author_name}</p>
-                <p class="card-text">${book.publish_date}</p>
-            </div>
-            <div class="card-footer">
-                <small class="text-muted">Last updated 3 mins ago</small>
-            </div>
-        </div>
-        `
-        searchResult.appendChild(div);
+        div.innerHTML = `
+                <div class="col">
+                    <div class="card h-100">
+                        <img src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" height="600vh"  class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">${book.title}</h5>
+                            <h6>Author Names:${book.author_name}</h6>
+                            <h6>Publish Date:${book.publish_date}</h6>
+                        </div>
+                        <div class="card-footer">
+                            <button onclick="authorDetails('${book.author_key}')" class="text-end rounded">Author Details</button>
+                        </div>
+                    </div>
+                 </div>
+        `;
+        displayAll.appendChild(div);
     });
 }
 
-
-
-
+const authorDetails = (authhorKey) => {
+    fetch(`https://openlibrary.org/authors/${authhorKey}.json`)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+        })
+}
